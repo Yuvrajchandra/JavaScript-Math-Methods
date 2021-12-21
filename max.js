5a75bf60e1049cd5e4e08bf06406d1ec7777b018
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 20:40:21 +0530

    added a math method

---
+++ max.js
@@ -0,0 +1,8 @@
+let list1 = [24, 645, 678, Infinity];
+let list2 = [75, 12, 92, 8];
+let list3 = [2947, -435, -123, 0, -Infinity];
+console.log(Math.max(...list1));
+console.log(Math.max(...list2));
+console.log(Math.max(...list3));
+console.log(Math.max(1, 2, 3));
+console.log(Math.max(234, 46, -23));
