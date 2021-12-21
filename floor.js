4ba94398f09013e598b47dd5b991ddbc9f5890be
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 20:34:22 +0530

    added a math method

---
+++ floor.js
@@ -0,0 +1,10 @@
+let num1 = 34.5;
+let num2 = 54.234;
+let num3 = 7.0001;
+let num4 = 867.1;
+let num5 = 0;
+console.log(Math.floor(num1));
+console.log(Math.floor(num2));
+console.log(Math.floor(num3));
+console.log(Math.floor(num4));
+console.log(Math.floor(num5));
