679fdd418a43986195242357dcc85614947144e5
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 21:00:35 +0530

    added a math method

---
+++ tanh.js
@@ -0,0 +1,10 @@
+let num1 = 0;
+let num2 = 1;
+let num3 = -1;
+let num4 = 2;
+let num5 = -2;
+console.log(Math.tanh(num1));
+console.log(Math.tanh(num2));
+console.log(Math.tanh(num3));
+console.log(Math.tanh(num4));
+console.log(Math.tanh(num5));
---
+++ trunc.js
@@ -0,0 +1,10 @@
+let num1 = 34.5;
+let num2 = 54.234;
+let num3 = 7.0001;
+let num4 = 867.1;
+let num5 = -0.7632;
+console.log(Math.trunc(num1));
+console.log(Math.trunc(num2));
+console.log(Math.trunc(num3));
+console.log(Math.trunc(num4));
+console.log(Math.trunc(num5));
