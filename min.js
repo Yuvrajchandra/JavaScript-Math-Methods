42839cb5d0c694e45b1ab0b749d5997924c30aa6
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 20:47:46 +0530

    added a math method

---
+++ min.js
@@ -0,0 +1,8 @@
+let list1 = [24, 645, 678, Infinity];
+let list2 = [75, 12, 92, 8];
+let list3 = [2947, -435, -123, 0, -Infinity];
+console.log(Math.min(...list1));
+console.log(Math.min(...list2));
+console.log(Math.min(...list3));
+console.log(Math.min(1, 2, 3));
+console.log(Math.min(234, 46, -23));
---
+++ pow.js
@@ -0,0 +1,5 @@
+console.log(Math.pow(1, 10));
+console.log(Math.pow(2, 4));
+console.log(Math.pow(1, Infinity));
+console.log(Math.pow(10, 10));
+console.log(Math.pow(-1, 3));
