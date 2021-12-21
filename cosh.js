a8e84d0f04302447faa8dbff6f1d4a6a75163f61
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 20:31:26 +0530

    added a math method

---
+++ cosh.js
@@ -0,0 +1,10 @@
+let num1 = 0;
+let num2 = 1;
+let num3 = -1;
+let num4 = 2;
+let num5 = -2;
+console.log(Math.cosh(num1));
+console.log(Math.cosh(num2));
+console.log(Math.cosh(num3));
+console.log(Math.cosh(num4));
+console.log(Math.cosh(num5));
---
+++ exp.js
@@ -0,0 +1,10 @@
+let num1 = 0;
+let num2 = 1;
+let num3 = -1;
+let num4 = 2;
+let num5 = -2;
+console.log(Math.exp(num1));
+console.log(Math.exp(num2));
+console.log(Math.exp(num3));
+console.log(Math.exp(num4));
+console.log(Math.exp(num5));
