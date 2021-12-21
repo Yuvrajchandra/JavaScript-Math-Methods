50af70347c823e70b2f2a9bd401b8fd2ec170ef1
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-12-21 20:15:11 +0530

    added a math method

---
+++ ceil.js
@@ -0,0 +1,10 @@
+let num1 = 34.5;
+let num2 = 54.234;
+let num3 = 7.0001;
+let num4 = 867.1;
+let num5 = 0;
+console.log(Math.ceil(num1));
+console.log(Math.ceil(num2));
+console.log(Math.ceil(num3));
+console.log(Math.ceil(num4));
+console.log(Math.ceil(num5));
